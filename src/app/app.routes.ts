62423460694d3dac2963807066d26d0b73fc6d58
Ilonczai András <ilonczai.andras16@gmail.com>
2025-07-02 14:03:40 +0200

    feat: add login component with form validation and styling
    style: adjust layout and styling for header, footer, and login components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/login/login.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     loadChildren: () =>
       import('./features/home/home.routes').then(m => m.HOME_ROUTES),
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: '',
