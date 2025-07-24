d3af03183ab7d297c5580865e86444d4f31539f0
Ilonczai András <ilonczai.andras16@gmail.com>
2025-07-24 22:31:50 +0200

    Refactor MyWorks feature: Remove old components and integrate header and body into a single new component with updated styles and functionality.
    Implement form handling for story details, including cover image upload, character management, and tag input. Enhance responsiveness and improve user experience.

--- src/app/features/myworks/myworks-new/myworks-new.component.ts
+++ src/app/features/myworks/myworks-new/myworks-new.component.ts
@@ -1,27 +1,205 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { RouterModule } from '@angular/router';
-import { MyWorksHeaderComponent } from './myworks-header/myworks-header.component';
-import { MyworksBodyComponent } from './myworks-body/myworks-body.component';
 import { LayoutService } from '../../../core/services/layout.service';
+import { CommonModule, Location } from '@angular/common';
+import { ProfileService } from '../../../core/services/profile.service';
+import { UserDto } from '../../../models/userDto';
+import { StoryRequestDto } from '../../../models/storyRequestDto';
+import { StoryService } from '../../../core/services/story.service';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CategoryLabels } from '../../../shared/enums/category.enum';
+import { LanguageLabels } from '../../../shared/enums/language.enum';
+import { CopyrightLicenseLabels } from '../../../shared/enums/copyright-license.enum';
+import { TargetAudienceLabels } from '../../../shared/enums/target-audience.enum';
 
 @Component({
   selector: 'app-myworks-new-page',
-  imports: [RouterModule, MyWorksHeaderComponent, MyworksBodyComponent],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './myworks-new.component.html',
   styleUrl: './myworks-new.component.css',
 })
 export class MyworksNewComponent implements OnInit {
   private destroy$ = new Subject<void>();
 
-  constructor(private layoutService: LayoutService) {}
+  profile!: UserDto | null;
+
+  storyRequest!: StoryRequestDto;
+
+  file!: any;
+
+  storyForm: FormGroup;
+  coverImageUrl: string | ArrayBuffer | null = null;
+
+  tagInputVisible = false;
+  tagInput: string = '';
+
+  categories = Object.values(StoryRequestDto.CategoryEnum);
+  categoryLabels = CategoryLabels;
+
+  languages = Object.values(StoryRequestDto.LanguageEnum);
+  languageLabels = LanguageLabels;
+
+  licenses = Object.values(StoryRequestDto.CopyrightEnum);
+  licenseLabels = CopyrightLicenseLabels;
+
+  audiences = Object.values(StoryRequestDto.TargetAudienceEnum);
+  audienceLabels = TargetAudienceLabels;
+
+  constructor(
+    private layoutService: LayoutService,
+    private profileService: ProfileService,
+    private storyService: StoryService,
+    private fb: FormBuilder,
+    private location: Location
+  ) {
+    this.storyForm = this.fb.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      characters: this.fb.array([this.fb.control('')]),
+      category: ['', [Validators.required]],
+      tags: this.fb.array([]),
+      targetAudience: ['', [Validators.required]],
+      language: ['', [Validators.required]],
+      copyright: ['', [Validators.required]],
+      mature: [false],
+    });
+  }
 
   ngOnInit(): void {
+    this.storyForm.valueChanges.subscribe((value) => {
+      this.setStoryManually();
+    });
+
     this.layoutService.setHeader(false);
+
+    this.profileService.profile$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((profile) => {
+        this.profile = profile;
+      });
   }
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  onSubmit() {
+    if (this.profile?.id)
+      if (this.isComplete) {
+        this.storyService
+          .createStory(this.profile.id, this.storyRequest, this.file)
+          .subscribe({
+            next: () => console.log('Story submitted'),
+            error: (err) => console.error(err),
+          });
+      }
+  }
+
+  onCoverSelected(event: any) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+
+      const MAX_SIZE_MB = 2;
+      if (file.size > MAX_SIZE_MB * 1024 * 1024) {
+        alert(`Image is too large. Max size is ${MAX_SIZE_MB}MB.`);
+        return;
+      }
+
+      this.coverImageUrl = URL.createObjectURL(file);
+    }
+  }
+
+  removeCharacter(index: number) {
+    (this.storyForm.get('characters') as FormArray).removeAt(index);
+  }
+
+  addCharacter() {
+    const charactersArray = this.storyForm.get('characters') as FormArray;
+    charactersArray.push(this.fb.control(''));
+  }
+
+  hasEmptyCharacter(): boolean {
+    return this.characters.some((char) => char.value.trim() === '');
+  }
+
+  removeTag(tagValue: string) {
+    const tagsArray = this.storyForm.get('tags') as FormArray;
+    const index = tagsArray.controls.findIndex(
+      (control) => control.value === tagValue
+    );
+
+    if (index >= 0) {
+      tagsArray.removeAt(index);
+    }
+  }
+
+  onTagInput(event: KeyboardEvent) {
+    if (event.key === ' ' && this.tagInput.trim() !== '') {
+      this.addTagFromInput();
+      event.preventDefault();
+    }
+  }
+
+  get isComplete(): boolean {
+    return this.storyService.isCompleteStory(this.storyRequest);
+  }
+
+  get characters(): FormControl[] {
+    return (this.storyForm.get('characters') as FormArray)
+      .controls as FormControl[];
+  }
+
+  get tags(): FormControl[] {
+    return (this.storyForm.get('tags') as FormArray).controls as FormControl[];
+  }
+
+  addTagFromInput() {
+    const trimmedTag = this.tagInput.trim();
+    if (trimmedTag) {
+      // Check if tag already exists in the FormArray
+      const tagsArray = this.storyForm.get('tags') as FormArray;
+      const existingTag = tagsArray.controls.find(
+        (control) => control.value === trimmedTag
+      );
+
+      if (!existingTag) {
+        tagsArray.push(this.fb.control(trimmedTag));
+      }
+    }
+    this.tagInput = '';
+  }
+
+  setStoryManually() {
+    const storyRequest: StoryRequestDto = {
+      title: this.storyForm.value.title.trim(),
+      description: this.storyForm.value.description.trim(),
+      mainCharacters: this.storyForm.value.characters.filter(
+        (char: string) => char.trim() !== ''
+      ),
+      category: this.storyForm.value.category,
+      tags: this.storyForm.value.tags,
+      targetAudience: this.storyForm.value.targetAudience,
+      language: this.storyForm.value.language,
+      copyright: this.storyForm.value.copyright,
+      mature: this.storyForm.value.mature,
+      coverImageUrl: this.coverImageUrl ? this.coverImageUrl.toString() : '',
+    };
+
+    this.storyService.setStory(storyRequest);
   }
 }
