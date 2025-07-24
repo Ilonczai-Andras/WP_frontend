603017bf0c04bd7887a9bd2f0f5f8732d11e16a2
Ilonczai András <ilonczai.andras16@gmail.com>
2025-07-24 13:25:18 +0200

    Implement back navigation in My Works header component and clean up button formatting

--- src/app/features/myworks/myworks-new/myworks-header/myworks-header.component.ts
+++ src/app/features/myworks/myworks-new/myworks-header/myworks-header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-myworks-header',
   imports: [],
   templateUrl: './myworks-header.component.html',
-  styleUrl: './myworks-header.component.css'
+  styleUrl: './myworks-header.component.css',
 })
 export class MyWorksHeaderComponent {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back();
+  }
 }
