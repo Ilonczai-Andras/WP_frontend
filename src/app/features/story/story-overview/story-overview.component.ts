b010d769fecd6a8c95feb2e72717e3cdc73fe7f6
Ilonczai András <ilonczai.andras16@gmail.com>
2025-07-25 17:42:47 +0200

    Add StoryOverview component with HTML, CSS, and TypeScript implementation for displaying story details and user interactions

---
+++ src/app/features/story/story-overview/story-overview.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { StoryResponseDto } from '../../../models/storyResponseDto';
+import { ProfileService } from '../../../core/services/profile.service';
+import { StoryService } from '../../../core/services/story.service';
+import { UserDto } from '../../../models/userDto';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-story-overview',
+  imports: [CommonModule],
+  templateUrl: './story-overview.component.html',
+  styleUrl: './story-overview.component.css',
+})
+export class StoryOverviewComponent {
+  profile!: UserDto | null;
+  isOwnProfile = false;
+
+  story: StoryResponseDto | null = {};
+
+  constructor(
+    private profileService: ProfileService,
+    private storyService: StoryService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const param = this.route.snapshot.paramMap.get('storyIdAndTitle');
+    const id = param ? Number(param.split('-')[0]) : 0;
+
+    this.profileService.profile$.subscribe((profile) => {
+      this.profile = profile;
+    });
+
+    this.profileService.isOwnProfile$.subscribe((isOwn) => {
+      this.isOwnProfile = isOwn;
+    });
+
+    this.storyService.getStory(id).subscribe((story) => {
+      this.story = story;
+      console.log(story);
+    });
+  }
+
+  startReading(): void {}
+
+  showMore(): void {}
+}
